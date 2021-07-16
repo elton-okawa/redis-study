ac8c975ee33b1472f481fed2c974961347e0c137
Elton Okawa <elton.okawa@gmail.com>
2021-07-16 06:58:33 -0300

    feat: add expireAt

--- src/index.js
+++ src/index.js
@@ -4,41 +4,40 @@ const redis = new Redis();
 redis.defineCommand("allocate", {
   numberOfKeys: 3,
   lua: `
-    local start = redis.call('TIME')
-
     local safety = KEYS[1]
     local groupHashKey = KEYS[2]
     local lastGroupKey = KEYS[3]
     local baseGroupKey = ARGV[1]
     local maxGroupSize = tonumber(ARGV[2])
-    
+    local expireAt = tonumber(ARGV[3])
+    redis.log(redis.LOG_NOTICE, expireAt)
+
     local pos
+    local groupKey
     if (redis.call('exists', safety) == 1) then
 
+      -- Maybe do it on league creation
       if (redis.call('exists', lastGroupKey) == 0) then
         redis.call('incr', lastGroupKey)
+        redis.call('expireat', lastGroupKey, expireAt)
+
+        redis.call('hset', groupHashKey, 'name', baseGroupKey)
+        redis.call('expireat', groupHashKey, expireAt)
       end
 
       local groupNumber = redis.call('get', lastGroupKey)
-      local groupKey = baseGroupKey .. '-' .. groupNumber
+      groupKey = baseGroupKey .. '-' .. groupNumber
 
       pos = redis.call('hincrby', groupHashKey, groupKey, 1)
 
-      redis.log(redis.LOG_NOTICE, pos)
-      redis.log(redis.LOG_NOTICE, maxGroupSize)
-
       if (pos >= maxGroupSize) then
         redis.call('incr', lastGroupKey)
+        redis.call('hdel', groupHashKey, groupKey)
       end
+
+      -- Caution: Lua table array like
+      return { groupKey, pos }
     end
-
-    local finish = redis.call('TIME')
-    redis.log(redis.LOG_NOTICE, start[1] .. ' ' .. start[2])
-    redis.log(redis.LOG_NOTICE, finish[1] .. ' ' .. finish[2])
-    local elapsedTimeUs = tonumber(finish[1]-start[1]) / 1000000 + tonumber(finish[2]-start[2])
-    redis.log(redis.LOG_NOTICE, 'Execution time: ' .. elapsedTimeUs .. 'us')
-
-    return pos
   `,
 });
 
@@ -50,16 +49,20 @@ SET safe ok
   */
 
   // hash table -> tira a necessidade de saber a chave em compile time
+  // expireAt -> seconds
 
   const baseGroupKey = 'group';
   const lastGroupKey = 'last-group';
   const groupHashKey = 'group-hash';
   const safeKey = 'safe';
 
-  const pos = await redis.allocate(safeKey, groupHashKey, lastGroupKey, baseGroupKey, 3)
+  const expireAt = Math.floor((Date.now() + 60000) / 1000);
+  console.log('ExpireAt: ' + expireAt);
+  const allocation = await redis.allocate(safeKey, groupHashKey, lastGroupKey, baseGroupKey, 3, expireAt)
   
-  if (pos !== null) {
-    console.log('Position: ' + pos);
+  if (allocation !== null) {
+    console.log('GroupKey: ' + allocation[0]);
+    console.log('Position: ' + allocation[1]);
     
     const lastGroup = await redis.get(lastGroupKey);
     const groupHash = await redis.hgetall(groupHashKey);
